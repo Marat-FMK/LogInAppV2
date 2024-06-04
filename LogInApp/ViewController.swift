8255d15cf3df8585e920bcee861822a5ce383704
MaratXcode <kosjachok8@yandex.ru>
2024-06-04 05:03:50 +0300

    worc with log in button

--- LogInApp/ViewController.swift
+++ LogInApp/ViewController.swift
@@ -19,14 +19,39 @@ class ViewController: UIViewController {
     }
 
     @IBAction func logInPasswordButton() {
-        guard let name = nameTextField.text else { return }
+        if nameTextField.text != "Marat" || passwordTextField.text != "qwerty" {
+            
+            let alert = UIAlertController(title: "Pass or Login !!!", message: "write correct data", preferredStyle: .alert)
+            
+            let goodAction = UIAlertAction(title: "Good", style: .default)
+            alert.addAction(goodAction)
+            present(alert, animated: true)
+        }
+        
     }
+    
+    
+    
+    
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let name = nameTextField.text else { return }
+        guard let pass = passwordTextField.text else { return }
+        
         guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
         welcomeVC.welcomeL = " Welcome, \(name)"
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     @IBAction func showName() {
         let alert = UIAlertController(title: "OMG , do you know where you're going ?", message: "write - @ Marat @", preferredStyle: .alert)
@@ -38,7 +63,7 @@ class ViewController: UIViewController {
     
     
     @IBAction func showPassword() {
-        let alert = UIAlertController(title: "You reele don`t understand your password?", message: "You pass : 12345", preferredStyle: .alert  )
+        let alert = UIAlertController(title: "You reele don`t understand your password?", message: "You pass : qwerty ", preferredStyle: .alert  )
         let understandAction = UIAlertAction(title: "understand", style: .default)
         alert.addAction(understandAction)
         present(alert, animated: true)
